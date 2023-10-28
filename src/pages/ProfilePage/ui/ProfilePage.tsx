9b3fbfc24bc2869646000bbb3b9db160a69d5d1d
sadside <sadside1319@gmail.com>
2023-10-29 00:55:55 +0700

    123

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,7 +1,7 @@
 import styles from './ProfilePage.module.scss';
 import {FC} from 'react';
-import {Achivment} from "src/widgets/achivment/ui/Achivment.tsx"
-import {Team} from "src/widgets/team/ui/Team.tsx"
+import {Achivment} from 'src/widgets/achivment/ui/Achivment.tsx';
+import {Team} from 'src/widgets/team/ui/Team.tsx';
 
 interface ProfilePageProps {
     className?: string;
@@ -11,49 +11,41 @@ export const ProfilePage: FC<ProfilePageProps> = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.profileSquares}>
-                    <table className ={styles.t1}>
-                        <tr>
-                            <td className ={styles.up}>
-                                <div className ={styles.teamHolder}>
-                                    <div className={styles.header}>team</div>
-                                    <div className={styles.border}></div>
-                                </div>
-                                <div className ={styles.achivmentHolder}>
-                                    <div className={styles.header}>achivment</div>
-                                    <div className={styles.border}></div>
-                                    <div className={styles.achScroll}>
-                                        <Achivment/>
-                                        <Achivment/>
-                                        <Achivment/>
-                                        <Achivment/>
-                                        <Achivment/>
-                                        <Achivment/>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <div className ={styles.rateHolder}>
-                                    <div className={styles.header}>rating</div>
-                                    <div className={styles.border}></div>
-                                    <div className={styles.rateScroll}>
-                                        <Team/>
-                                        <Team/>
-                                        <Team/>
-                                        <Team/>
-                                        <Team/>
-                                        <Team/>
-                                    </div>
-                                </div>
-                                <div className ={styles.memeHolder}>
-                                    <div className={styles.header}>meme</div>
-                                    <div className={styles.border}></div>
-                                    <p className={styles.contacts}>
-                                        some contacts(probably mine)
-                                    </p>
-                                </div>                
-                            </td>
-                        </tr>
-                    </table>    
+                <div className={styles.teamHolder}>
+                    <div className={styles.header}>team</div>
+                    <div className={styles.border}></div>
+                </div>
+                <div className={styles.achivmentHolder}>
+                    <div className={styles.header}>achivment</div>
+                    <div className={styles.border}></div>
+                    <div className={styles.achScroll}>
+                        <Achivment />
+                        <Achivment />
+                        <Achivment />
+                        <Achivment />
+                        <Achivment />
+                        <Achivment />
+                    </div>
+                </div>
+                <div className={styles.rateHolder}>
+                    <div className={styles.header}>rating</div>
+                    <div className={styles.border}></div>
+                    <div className={styles.rateScroll}>
+                        <Team />
+                        <Team />
+                        <Team />
+                        <Team />
+                        <Team />
+                        <Team />
+                    </div>
+                </div>
+                <div className={styles.memeHolder}>
+                    <div className={styles.header}>meme</div>
+                    <div className={styles.border}></div>
+                    <p className={styles.contacts}>
+                        some contacts(probably mine)
+                    </p>
+                </div>
             </div>
         </div>
     );
